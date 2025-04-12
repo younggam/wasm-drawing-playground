a235852dbd7f9d46c1bf4101aa833fbcf7410bc1
younggam <nainjoo2001@naver.com>
2025-04-12 19:32:46 +0900

    404 error fix

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,16 +2,24 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
-  experiments: {
-    asyncWebAssembly: true,
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
-  },
-  mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+    entry: "./bootstrap.js",
+    experiments: {
+        asyncWebAssembly: true,
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bootstrap.js",
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+        ],
+    },
+    plugins: [
+        new CopyWebpackPlugin(['index.html', 'icons/*.svg'])
+    ],
 };
